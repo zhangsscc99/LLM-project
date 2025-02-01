0c6585ff44cfbb27c1312842aa185ccbe64e9e13
Yuting Wang <yw4554@nyu.edu>
2025-01-31 21:50:39 -0600

    10

--- src/page/component/introParagraph.vue
+++ src/page/component/introParagraph.vue
@@ -1,9 +1,11 @@
 <template>
     <div class="main-header">
-        <div class="main-header-left"></div>
-        <p class="name-field">Hi 我是你的旅游助手</p>
-        <p class="intro-section">你的专属旅游助手，可以帮你规划行程和解答出游问题哦</p>
-        <div>
+        <div class="main-header-left">
+            <p class="name-field">Hi 我是你的旅游助手</p>
+            <p class="intro-section">你的专属旅游助手，可以帮你规划行程和解答出游问题哦</p>
+        </div>
+        <div class="main-header-right">
+
             <img src="@/assets/intro-section.png" alt="">
         </div>
     </div>
--- src/page/home/home.vue
+++ src/page/home/home.vue
@@ -1,16 +1,7 @@
 <template>
     <div class="content">
-        123
+        <introParagraph></introParagraph>
     </div>
-    <div>
-        <van-button type="primary">主要按钮</van-button>
-        <van-button type="success">成功按钮</van-button>
-        <van-button type="default">默认按钮</van-button>
-        <van-button type="danger">危险按钮</van-button>
-        <van-button type="warning">警告按钮</van-button>
-        <p class="hello">你好</p>
-    </div>
-
 </template>
 
 <script setup lang="ts">
