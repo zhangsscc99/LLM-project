55503aef69ee62d494c9f9ba949047ee40364d86
Yuting Wang <yw4554@nyu.edu>
2025-02-01 10:52:36 -0600

    11

---
+++ src/page/component/defaultQuestion.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="support-agent">
+        <img src="@/assets/kefu.png" alt="">
+        <p>旅游助手</p>
+    </div>
+    <div class="customer-service">
+        <span class="tips">你可以这样问我</span>
+        <div class="customer-service-list">
+            <img src="@/assets/wenti.png" alt="">
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped lang="less">
+.support-agent{
+    display: flex;
+    align-items: center;
+    img{
+        object-fit: cover;
+        width: 35px;
+        height: 35px;
+        margin-right: 8px;
+    }
+
+    p{
+        font-size: 14px;
+        
+    }
+}
+.customer-service{
+    background-color:#ffffff; 
+    margin: 10px 0;
+    padding: 10px; 
+    border-top-right-radius: 10px;
+    border-bottom-right-radius: 10px;
+    border-bottom-left-radius: 10px;
+}
+</style>
+
--- src/page/home/home.vue
+++ src/page/home/home.vue
@@ -1,11 +1,13 @@
 <template>
     <div class="content">
         <introParagraph></introParagraph>
+        <defaultQuestion />
     </div>
 </template>
 
 <script setup lang="ts">
 import introParagraph from '@/page/component/introParagraph.vue'
+import defaultQuestion from '@/page/component/defaultQuestion.vue'
 </script>
 
 
