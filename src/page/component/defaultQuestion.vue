c3bf51ea7c8665c17608872c1035bdd440fff2c9
Yuting Wang <yw4554@nyu.edu>
2025-02-01 10:59:27 -0600

    11

--- src/page/component/defaultQuestion.vue
+++ src/page/component/defaultQuestion.vue
@@ -7,7 +7,12 @@
         <span class="tips">你可以这样问我</span>
         <div class="customer-service-list">
             <img src="@/assets/wenti.png" alt="">
+            <p>你好</p>
         </div>
+    </div>
+    <div class="change-section">
+        <img src="@/assets/huanyihuan.png" alt="">
+        <p>换一换</p>
     </div>
 </template>
 
@@ -38,6 +43,45 @@
     border-top-right-radius: 10px;
     border-bottom-right-radius: 10px;
     border-bottom-left-radius: 10px;
+    .tips {
+        font-size: 16px;
+
+    }
+    .customer-service-list{
+        display: flex;
+        align-items: center;
+        background-color: #f2f4ff;
+        border-radius: 40px;
+        margin-top: 10px;
+        padding: 5px 7px; 
+        img{
+            width: 25px;
+            height: 25px;
+            object-fit: cover;
+        }
+        p{
+            font-size: 14px;
+            padding-left: 4px;
+        }
+    }
+}
+.change-section{
+    display: inline-flex;
+    align-items: center; 
+    background-color: #a8abb0;
+    border-radius: 40px; 
+    padding: 4px 7px;
+    img{
+        width: 20px; 
+        height: 20px;
+
+    }
+    p{
+        font-size: 14px; 
+        padding-left: 7px;
+        color: #ffffff;
+        
+    }
 }
 </style>
 
