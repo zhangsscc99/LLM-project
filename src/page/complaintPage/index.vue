a05e0f8e943034414ea965ac878dbc9ccaae5e44
Yuting Wang <yw4554@nyu.edu>
2025-02-12 01:15:40 -0600

    21

--- src/page/complaintPage/index.vue
+++ src/page/complaintPage/index.vue
@@ -1,7 +1,7 @@
 <template>
     <van-nav-bar title="投诉" left-arrow fixed placeholder/>
-    <div class="complaint-container">
-        <p>你要投诉谁</p>
+    <div class="complaint-page">
+        <p class="title-text">你要投诉谁</p>
         <van-field
     is-link
     readonly 
@@ -14,12 +14,31 @@
 </van-popup>
 
     </div>
+    <div class="complaint-page">
+        <p class="title-text">请告诉我们发生了什么事</p>
+        <van-field type="textarea" class="input-content" placeholder="输入投诉原因"></van-field>
+    </div>
+    <div class="complaint-page">
+        <p class="title-text">这件事发生在哪里</p>
+        <van-field
+    is-link
+    readonly 
+    placeholder="选择地区" @click="showPickerCity = true" />
+
+<van-popup :show="showPickerCity" round position="bottom">
+    <van-picker
+    :columns="cityData" @cancel="showPickerCity = false" />
+    
+</van-popup>
+
+    </div>
     
 
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue';
+import { cityData } from '@/config/city';
 // 投诉对象数据
 const objectData = ref([
     {
@@ -37,6 +56,19 @@ const objectData = ref([
 
 ])
 const showPicker = ref(false)
+const showPickerCity = ref(false)
 </script>
 <style scoped lang="less">
+.complaint-page{
+    padding: 10px;
+    .title-text{
+        padding-bottom: 10px;
+        font-weight: bold;
+    }
+    .input-content{
+        background-color: #f8f9fd;
+        border-radius: 10px;
+        padding: 6px;
+    }
+}
 </style>
