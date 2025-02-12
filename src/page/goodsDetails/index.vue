9cff26e7044ad615499019ca4bbc3645e66df570
Yuting Wang <yw4554@nyu.edu>
2025-02-12 01:44:12 -0600

    22

--- src/page/goodsDetails/index.vue
+++ src/page/goodsDetails/index.vue
@@ -1,9 +1,34 @@
 <template>
-    <p>商品详情页</p>
-
+    <van-nav-bar title="商品详情" left-arrow fixed placeholder/>
+    <img class="cover-image" src="https://cn.matsuyama-sightseeing.com/wp-content/themes/MatsuyamaSightseeing_theme_2019_cn/assets/img/home/kvsp_05.jpg" alt="">
+    <div class="price-and-title">
+        <p class="goods-price">199/人</p>
+        <p class="goods-title">标题</p>
+    </div>
 </template>
 
 <script setup lang="ts">
 </script>
 <style scoped lang="less">
+.cover-image{
+    width: 100%;
+    max-height: 300px;
+    object-fit: cover;
+
+}
+.price-and-title{
+    background-color: #ffffff;
+    margin: 10px;
+    padding: 10px;
+    border-radius: 10px;
+    .goods-price{
+        font-size: 22px;
+        color: red;
+        padding-bottom: 10px;
+    }
+    .goods-title{
+        font-size: 19px;
+    }
+    
+}
 </style>
