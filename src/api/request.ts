f67796706c65cc6b7cd27f9e9ca90439b329ff77
Yuting Wang <yw4554@nyu.edu>
2025-02-26 19:02:33 -0600

    fetch api writing

--- src/api/request.ts
+++ src/api/request.ts
@@ -1,6 +1,22 @@
 import { chatMessageType } from "@/types/index"
+const requestUrl = "http://127.0.0.1:7000"
+
 
 // fetch请求
+const fetchApi = async(url:string, method:"POST" | "GET", body?:any, resType="seream", reqType="json")=>{
+    const headers:HeadersInit={
+        ...(reqType == 'json' && {"Content-Type":"application/json"})
+    }
+    let bodyData = null 
+    if(reqType == 'json'){
+        bodyData = JSON.stringify(body)
+    }else if(method == 'GET'){
+        bodyData = null
+    }else{
+        bodyData = body
+    }
+    await fetch(requestUrl)
+}
 
 // 统一返回的接口
 interface ApiResponse<T>{
