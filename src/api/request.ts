9f257d092955a1efe780d293c5cfe980641ca206
Yuting Wang <yw4554@nyu.edu>
2025-02-26 18:36:08 -0600

    fetch api

--- src/api/request.ts
+++ src/api/request.ts
@@ -1,2 +1,17 @@
+import { chatMessageType } from "@/types/index"
+
+// fetch请求
+
+// 统一返回的接口
+interface ApiResponse<T>{
+    data:T, //返给前端的数据
+    msg:string, //提示
+    error:any, //错误说明
+    serviceCode, //业务错误码
+    code //状态码
+}
 // 发送消息
-export const chatMessageApi = 
+export const chatMessageApi = (data:{chatMessage:chatMessageType}):Promise<ApiResponse<Buffer>>
+
+
+
--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,6 +1,17 @@
 export type TextContent = {type:'text', text:string}
 export type ImageContent = {type:'image_url', image_url:{url:string}}
 
+
+// 搜索商品返回的结果
+export type ServerSearchGoodsType = {
+    _id:string;
+    coverImage:string;
+    contentTitle:string;
+    price:number
+}[]
+
+
+
 // 发送消息
 export type chatMessageType = {
     role:"user" | "assistant" // 角色
@@ -8,5 +19,9 @@ export type chatMessageType = {
     toolData?:any;//存储返回的函数调用工具数据
     type?:"function";//是单纯对话还是含有工具调用的数据
     functionName?:string;
+    progress?:boolean;//返回进度, false请求中, true数据已返回
+    searchGoodsData?:ServerSearchGoodsType[];//搜索商品返回的结果
+
+
 
 }
