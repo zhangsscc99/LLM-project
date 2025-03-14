aaece12ef1cd207b7726ac38322fed3b771c705e
Yuting Wang <yw4554@nyu.edu>
2025-03-13 20:29:41 -0500

    69

--- src/page/component/defaultQuestion.vue
+++ src/page/component/defaultQuestion.vue
@@ -5,19 +5,54 @@
     </div>
     <div class="customer-service">
         <span class="tips">你可以这样问我</span>
-        <div class="customer-service-list">
-            <img src="@/assets/wenti.png" alt="">
-            <p>你好</p>
+        <div class="customer-service-list" v-for="(item, index) in customerServiceList" :key="index"
+        @click="selectSend(item)"
+        >
+            <img src="@/assets/wenti.png" alt="" />
+            <p>{{ item }}</p>
         </div>
         
     </div>
-    <div class="change-section">
+    <div class="change-section" @click="switchMode">
         <img src="@/assets/huanyihuan.png" alt="">
         <p>换一换</p>
     </div>
 </template>
 
 <script setup lang="ts">
+import { ref, computed } from "vue";
+import { chatbotMessage } from "@/store/index";
+const store = chatbotMessage();
+// 默认问题
+const customerServiceList = ref([
+    "哀牢山可以进入探险旅游吗",
+    "大理古城有哪些夜市",
+    "大理古城的门票是多少",
+    "大理古城的开放时间是什么时候",
+    "大理古城的地址在哪里",
+    "大理古城的联系电话是多少",
+    "大理古城的交通方式有哪些",
+    "大理古城的住宿有哪些",
+]);
+const currentIndex = ref(0);
+const displayedList = computed<string[]>(()=>{
+    return JSON.parse(JSON.stringify(customerServiceList.value)).slice(currentIndex.value, 5);
+});
+// 换一换
+let grop = 0
+const switchMode = ()=>{
+    grop + 5
+    if (grop >= customerServiceList.value.length){
+        currentIndex.value = 0;
+        grop = 0;
+        return
+    }
+    currentIndex.value += 5;
+
+};
+const selectSend = (val:string)=>{
+    store.sendMessage(val);
+}
 
 </script>
 
