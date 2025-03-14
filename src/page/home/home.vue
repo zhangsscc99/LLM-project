0e3329f4a4d7150ec41d14f57297a214bf6ae357
Yuting Wang <yw4554@nyu.edu>
2025-03-13 20:52:32 -0500

    69

--- src/page/home/home.vue
+++ src/page/home/home.vue
@@ -13,7 +13,36 @@
 import introParagraph from '@/page/component/introParagraph.vue'
 import defaultQuestion from '@/page/component/defaultQuestion.vue'
 import chatMessage from '@/page/component/chatMessage.vue'
+// 底部输入框
 import inputArea from '@/page/component/inputArea.vue'
+import { watch, onMounted } from 'vue';
+import { chatbotMessage } from '@/store/index';
+const store = chatbotMessage();
+// 监听滚动，对话输出自动滚动底部
+watch(
+    ()=>store.messages, 
+    (val)=>{
+        automatic();
+    }, 
+    {deep:true}
+);
+const automatic = ()=>{
+    const contentElement = document.querySelector(".content")
+    window.scrollTo({
+        top: contentElement?.scrollHeight,
+        behavior: "smooth",
+    });
+};
+onMounted(()=>{
+    // 监听滚动事件
+    window.addEventListener("scroll", )
+});
+// 监听滚动方向
+const handleScroll = ()=>{
+    const currentwindow.scrollY > 100 ? 
+}
+
+
 </script>
 
 
